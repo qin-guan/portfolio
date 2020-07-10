b8b6b5bc060d944b9a17e39a5d1171bc6b504096
qinguan <qinguan20040914@gmail.com>
2020-07-10 23:50:28 +0800

    Updated content on landing page and temporarily set theme to dark

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,15 +33,9 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
           <Row>
             <Col xs={24} sm={24} md={24} lg={18} xl={18}>
               <Space direction="vertical" size={"large"}>
-                <motion.div initial={{
-                  opacity: 0
-                }} animate={{
-                  opacity: 1
-                }}>
-                  <Title style={{
-                    fontFamily: "Metropolis"
-                  }}>Hi, I'm Qin Guan</Title>
-                </motion.div>
+                <Title style={{
+                  fontFamily: "Metropolis"
+                }}>Hi, I'm Qin Guan</Title>
                 <Space>
                   <a href={"https://github.com/qin-guan"} target={"_blank"}>
                     <GithubOutlined style={{
@@ -80,17 +74,11 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
           </Footer>
         </Divider>
         <Content>
-          <motion.div initial={{
-            opacity: 0
-          }} animate={{
-            opacity: 1
+          <Title style={{
+            fontFamily: "Metropolis"
           }}>
-            <Title style={{
-              fontFamily: "Metropolis"
-            }}>
-              My experiences
-            </Title>
-          </motion.div>
+            My experiences
+          </Title>
           <Timeline>
             <Timeline.Item>
               <Title style={{
@@ -98,10 +86,12 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
                 fontWeight: 500
               }} level={4}>
                 <Text keyboard>2017-Present</Text>
-                Co-Founder & CTO, CodeCollab.io
+                Co-Founder & CTO at CodeCollab.io
               </Title>
               <Text>
-                Together with Carl, my classmate, we created CodeCollab. A collaborative code editor that allows anyone
+                Together with Carl, my classmate, we created <a href={"https://codecollab.io"}
+                                                                target={"_blank"}>CodeCollab</a>. A collaborative code
+                editor that allows anyone
                 to compile, collaborate and create code anywhere, at any time.
               </Text>
             </Timeline.Item>
@@ -111,10 +101,12 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
                 fontWeight: 500
               }} level={4}>
                 <Text keyboard>2018</Text>
-                Swift Accelerator Program (2018)
+                Swift Accelerator Program (Participant)
               </Title>
               <Text>
-                I am part of the pioneering batch of the <a href={"https://swiftinsg.org"} target={"_blank"}>Swift Accelerator Program</a>. The program, by IMDA and Apple, covers skills such as building apps with Swift and also marketing and design principles.
+                I am part of the pioneering batch of the <a href={"https://swiftinsg.org"} target={"_blank"}>Swift
+                Accelerator Program</a>. The program, by IMDA and Apple, covers skills such as building apps with Swift
+                and also marketing and design principles.
               </Text>
             </Timeline.Item>
             <Timeline.Item>
@@ -123,7 +115,21 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
                 fontWeight: 500
               }} level={4}>
                 <Text keyboard>2018</Text>
-                Chief Technology Officer (iOS), SST Inc</Title>
+                Swift Innovators' Summit
+              </Title>
+              <Text>
+                As part of the <a href={"https://swiftinsg.org"} target={"_blank"}>Swift
+                Accelerator Program</a>, Carl and I created an app, Parkgoer, and presented it at the Summit to IMDA
+                Assistant Chief Executive of Planning, Ms Koh Li-Na.
+              </Text>
+            </Timeline.Item>
+            <Timeline.Item>
+              <Title style={{
+                fontFamily: "Inter",
+                fontWeight: 500
+              }} level={4}>
+                <Text keyboard>2018</Text>
+                Chief Technology Officer (iOS) at SST Inc</Title>
               <Text>
                 As the Chief Technology Officer for iOS, I assisted students in developing their iOS applications. I
                 also maintained SST Inc's iOS resources, such as deploying apps to the app store.
@@ -134,8 +140,33 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
                 fontFamily: "Inter",
                 fontWeight: 500
               }} level={4}>
+                <Text keyboard>2019-2020</Text>
+                Swift Accelerator Program (Volunteer)</Title>
+              <Text>
+                After taking part in the Swift Accelerator Program, I volunteered to to help the subsequent cohorts and
+                provided technical assistance.
+              </Text>
+            </Timeline.Item>
+            <Timeline.Item>
+              <Title style={{
+                fontFamily: "Inter",
+                fontWeight: 500
+              }} level={4}>
                 <Text keyboard>2019</Text>
-                Chief Technology Officer (React), SST Inc</Title>
+                Hackomania (Volunteer)</Title>
+              <Text>
+                I volunteered at Hackomania 2019, an annual 24-hour hackathon by Geekshacking Community that aims to
+                bring together tech enthusiasts, students and working professionals from various backgrounds to tackle
+                real-world issues with tech solutions. At the hackathon, I helped with event logistics.
+              </Text>
+            </Timeline.Item>
+            <Timeline.Item>
+              <Title style={{
+                fontFamily: "Inter",
+                fontWeight: 500
+              }} level={4}>
+                <Text keyboard>2019</Text>
+                Chief Technology Officer (React) at SST Inc</Title>
               <Text>
                 As the Chief Technology Officer for React, I promoted the use of React/React Native to students and also
                 assisted them in getting started with the framework. I conducted a 5 day course on React Native, which
@@ -147,13 +178,24 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
                 fontFamily: "Inter",
                 fontWeight: 500
               }} level={4}>
-                <Text keyboard>2019</Text>
-                Software Engineer, Outside Technologies Private Limited</Title>
+                <Text keyboard>2019-Present</Text>
+                Software Engineer at Outside Technologies Private Limited</Title>
               <Text>
-                At Outside, I worked on the <a href={"https://web.outsideapp.co"} target={"_blank"}>web app</a> that is in use today. I also helped to manage our cloud platforms, namely Google Cloud Platform and Tencent Cloud. While developing the web app, I also implemented CI/CD into the workflow to help the team test and deploy at greater efficiency.
+                At Outside, I worked on our <a href={"https://web.outsideapp.co"} target={"_blank"}>web app</a> that is
+                in use today. I also helped to manage our cloud platforms, namely Google Cloud Platform and Tencent
+                Cloud. While developing the web app, I also implemented CI/CD into the workflow to help the team test
+                and deploy at greater efficiency.
               </Text>
             </Timeline.Item>
           </Timeline>
+        </Content>
+        <Content>
+          <Title style={{
+            fontFamily: "Metropolis"
+          }}>
+            My Projects
+          </Title>
+
         </Content>
       </Page>
     )
