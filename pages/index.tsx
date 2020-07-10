573a5667fed00aea839ab07b5738dc984d07a454
qinguan <qinguan20040914@gmail.com>
2020-07-10 12:15:01 +0800

    Added Typescript and Ant Design

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface IIndexPageState {
+}
+
+interface IIndexPageProps {
+}
+
+class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
+  constructor(props: IIndexPageProps) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+        <div />
+    );
+  }
+}
+
+export default IndexPage;
