5c91b7df576a44166ff4a4c8b3fd9ad28b9b71ad
Qin Guan <qinguan20040914@gmail.com>
2021-04-27 16:50:26 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -316,7 +316,7 @@ class IndexPage extends React.Component<IIndexPageProps, IIndexPageState> {
             </Flex>
             <Flex>
               <Space direction={"vertical"} size={"middle"}>
-                <Image src={"/pigeon.png"}/>
+                <Image src={"/pigeon.svg"}/>
                 <Title style={{
                   fontFamily: "Inter",
                   fontWeight: 500
