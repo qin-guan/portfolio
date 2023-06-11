1edbe6af1ce71dde30dff0d4311e46364cf0b65c
Qin Guan <qinguan20040914@gmail.com>
2023-06-11 18:26:20 +0800

    feat: use deep outline

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   description: "Cybersecurity student at Ngee Ann Polytechnic",
   lastUpdated: true,
   themeConfig: {
+    outline: 'deep',
     search: {
       provider: 'local'
     },
