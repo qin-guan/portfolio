7378d78afecdfd78d2e4ed8f41fc9035a220a42e
Qin Guan <qinguan20040914@gmail.com>
2023-07-26 11:44:35 +0800

    fix: disable mpa

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  mpa: true,
   title: "Qin Guan",
   description: "Cybersecurity student at Ngee Ann Polytechnic",
   lastUpdated: true,
