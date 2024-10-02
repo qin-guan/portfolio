b6654235572e9c8de751db3914ab2351626e5e57
Qin Guan <qinguan20040914@gmail.com>
2024-10-02 12:17:01 +0800

    Update Days.vue

---
+++ components/Days.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const days = daysBetween(new Date("2036-08-15"), new Date(Date.now()))
+
+function daysBetween(date1, date2) {
+    const ONE_DAY = 1000 * 60 * 60 * 24;
+    const differenceMs = Math.abs(date1 - date2);
+    return Math.round(differenceMs / ONE_DAY);
+}
+
+</script>
+<template>
+
+	{{ days }} days
+
+</template>
